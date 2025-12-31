f8cba28f173c1c1fb2b045e20d2d5b0e8e802ad3
Des Albert <des.albert@gmail.com>
2025-12-31 10:11:23 -0800

    first commit

--- app/src/main/java/org/dba/scoreboard/ui/screens/HistoryScreen.kt
+++ app/src/main/java/org/dba/scoreboard/ui/screens/HistoryScreen.kt
@@ -17,7 +17,6 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBackIos
-import androidx.compose.material.icons.automirrored.filled.ArrowForwardIos
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
