0571b5948449566886d99893ff14caf7401b5c38
Des Albert <des.albert@gmail.com>
2026-01-01 09:54:46 -0800

    updates

--- app/src/main/java/org/dba/scoreboard/MainActivity.kt
+++ app/src/main/java/org/dba/scoreboard/MainActivity.kt
@@ -6,9 +6,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -76,6 +74,7 @@ fun TopBar() {
         )
     )
 }
+
 
 sealed class Screen(val route: String) {
     object Home : Screen("home")
